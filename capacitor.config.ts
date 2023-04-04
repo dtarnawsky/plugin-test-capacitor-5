5d4441703457e345552cc5991bfa124bd823512a
Damian Tarnawsky <damian@ionic.io>
2023-04-04 09:45:23 -0700

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'plugin.test.capacitor.5',
+  appName: 'plugin-test-capacitor-5',
+  webDir: 'build'
+};
+
+export default config;
